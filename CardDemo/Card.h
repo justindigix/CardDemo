b474aec0ec262a69fb620cff545fa4b231e34654
justindigix <justin@justindemacbook-pro.home>
2016-08-17 15:20:16 +0200

    Add Card Class

---
+++ CardDemo/Card.h
@@ -0,0 +1,13 @@
+//
+//  Card.h
+//  CardDemo
+//
+//  Created by Justin Zhang on 17.08.16.
+//  Copyright © 2016 Tutorials. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Card : NSObject
+
+@end
---
+++ CardDemo/Card.m
@@ -0,0 +1,13 @@
+//
+//  Card.m
+//  CardDemo
+//
+//  Created by Justin Zhang on 17.08.16.
+//  Copyright © 2016 Tutorials. All rights reserved.
+//
+
+#import "Card.h"
+
+@implementation Card
+
+@end
